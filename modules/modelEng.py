ee5af2038cc0f5488dbbd19c9e0a063ae518ef14
necronia <necronia44@gmail.com>
2019-08-19 12:42:03 -0400

    change ratio

--- modules/modelEng.py
+++ modules/modelEng.py
@@ -7,33 +7,35 @@ from sklearn.metrics import mean_squared_error, make_scorer
 
 def get_param(model):
     cv = 5
-    n_jobs = 8
+    n_jobs = -1
     verbose = 1
     scoring = make_scorer(mean_squared_error, greater_is_better=False)
     
-    if type(model).__name__ == 'ElasticNet':        
+    class_name = type(model).__name__ 
+    
+    if class_name == 'ElasticNet':        
         params = {'alpha': [0.0003, 0.0004, 0.0005],
                   'l1_ratio': [0.9, 0.95, 0.99, 1],
                   'max_iter': [10000]}
         return cv, n_jobs, verbose, scoring, params
-    elif type(model).__name__ == 'RandomForestRegressor':
+    elif class_name == 'RandomForestRegressor':
         params = {'max_depth': [3, 4, 5],
                   'min_samples_leaf': [3, 4, 5],
                   'n_estimators': [700,1000,1460]}
         return cv, n_jobs, verbose, scoring, params
-    elif type(model).__name__ == 'Lasso':
+    elif class_name == 'Lasso':
         params = {'alpha': [0.0005],
                  "normalize": [False]}
         return cv, n_jobs, verbose, scoring, params
-    elif type(model).__name__ == 'Ridge':
+    elif class_name == 'Ridge':
         params = {'alpha': [10, 11]}
         return cv, n_jobs, verbose, scoring, params
-    elif type(model).__name__ == 'SVR':
+    elif class_name == 'SVR':
         params = {'gamma': ['scale'],
                  'C': [10000],
                  'epsilon': [0.1]}
         return cv, n_jobs, verbose, scoring, params
-    elif type(model).__name__ == 'XGBRegressor':
+    elif class_name == 'XGBRegressor':
         params = {'learning_rate': [0.05],
                   'max_depth': [5],
                   'subsample': [0.9],
@@ -42,7 +44,7 @@ def get_param(model):
                   'n_estimators':[3000],
                   'refit' : [True]}
         return cv, n_jobs, verbose, scoring, params
-    elif type(model).__name__ == 'LGBMRegressor':
+    elif class_name == 'LGBMRegressor':
         params = {'objective':['regression'],
                   'num_leave' : [1],
                   'learning_rate' : [0.05],
@@ -58,7 +60,7 @@ def grid_search(train_X, train_Y, model):
     grid_model = GridSearchCV(model, param_grid=params, scoring=scoring, cv=kf, verbose=verbose, n_jobs=n_jobs)
     grid_model.fit(train_X, train_Y)
     
-    print("Estimator: {} score: ({}) best params: {}".format(type(model).__name__, sqrt(-grid_model.best_score_), grid_model.best_params_))
+    print("Model: {} Score: ({}) Best params: {}".format(type(model).__name__, sqrt(-grid_model.best_score_), grid_model.best_params_))
     print(grid_model.best_estimator_)
     
     return grid_model.best_estimator_
